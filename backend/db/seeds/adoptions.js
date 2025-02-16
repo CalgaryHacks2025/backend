551985a8c351f434b88b0ea072d02eb112f066c1
Max Kamis <caumutkamis@gmail.com>
2025-02-16 03:18:22 -0700

    Fixed the database

--- backend/db/seeds/adoptions.js
+++ backend/db/seeds/adoptions.js
@@ -14,9 +14,7 @@
 export async function seed(knex) {
   await knex("adoptions").del();
   await knex("adoptions").insert([
-    { adoption_date: '2024-02-15', animal_id: 3, user_id: 1 }, 
-    { adoption_date: '2024-02-14', animal_id: 4, user_id: 2 },  
-    { adoption_date: '2024-02-16', animal_id: 6, user_id: 5 },  
-    { adoption_date: '2024-02-17', animal_id: 8, user_id: 7 },
+    { adoption_date: '2024-02-15', animal_id: 1, user_id: 1 }, 
+    { adoption_date: '2024-02-14', animal_id: 2, user_id: 2 },  
   ]);
 };
